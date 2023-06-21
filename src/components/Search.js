1e3e85efc9653fd32d89d428fcf6d337cb82736f
Cameron McEachin <cmceachin5888@gmail.com>
2023-06-21 01:52:09 -0400

    updated search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,55 +1,77 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { fetchPosts } from "../api";
+import { useLocation } from "react-router-dom";
 
-// export const searchFunc = ({ posts, isLoggedIn, setIsLoggedIn }) => {
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const token = localStorage.getItem("token");
+const SearchBar = () => {
+    const [posts, setPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const [isSearching, setIsSearching] = useState(false);
+    const searchLocation = useLocation();
+  
+    useEffect(() => {
+      const token = localStorage.getItem("token");
+  
+      const getPosts = async () => {
+        try {
+          const posts = await fetchPosts(token);
+          setPosts(posts);
+        } catch (error) {
+          console.error("Failed to fetch posts", error);
+        }
+      };
+  
+      getPosts();
+    }, []);
+  
+    const postMatches = (post, text) => {
+      return (
+        post.title.toLowerCase().includes(text.toLowerCase()) ||
+        post.description.toLowerCase().includes(text.toLowerCase()) ||
+        post.author.username.toLowerCase().includes(text.toLowerCase()) ||
+        post.price.toLowerCase().includes(text.toLowerCase()) ||
+        post.location.toLowerCase().includes(text.toLowerCase())
+      );
+    };
+  
+    const handleSearch = () => {
+      const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
+      setSearchResults(filteredPosts);
+      setIsSearching(true);
+    };
+    
+  const showSearchBar = searchLocation.pathname === "/";
 
-//   useEffect(() => {
-//     if (token) {
-//       setIsLoggedIn(true);
-//     }
-//   }, []);
-
-//   const postMatches = (post, text) => {
-//     return (
-//       post.title.includes(text) ||
-//       post.description.includes(text) ||
-//       post.author.username ||
-//       post.price ||
-//       post.location
-//     );
-//   };
-
-//   const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
-//   const postsToDisplay = searchTerm.length ? filteredPosts : posts;
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         name="Search"
-//         value={searchTerm}
-//         onChange={(event) => setSearchTerm(event.target.value)}
-//       />
-
-//       {/* {postsToDisplay.map((post) => (
-//         <div className="posts" key={post._id}>
-//           <h3>{post.title}</h3>
-//           <div>{post.author?.username || "Unknown User"}</div>
-//           <div>Description: {post.description}</div>
-//           <div>Price: {post.price}</div>
-//           <div>Location: {post.location}</div>
-//           <div>
-//             Will Deliver?:{" "}
-//             {post.willDeliver ? "Will Deliver" : "Will NOT deliver"}
-//           </div>
-//           {isLoggedIn && (
-//             <button onClick={() => handleViewPost(post._id)}>View Post</button>
-//           )}
-//         </div>
-//       ))} */}
-//     </div>
-//   );
-// };
-
-// export default searchFunc;
+  return (
+    <div>
+      {showSearchBar && (
+        <div>
+          <div>
+            <input
+              type="text"
+              name="Search"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+  
+          {isSearching && searchResults.length === 0 ? (
+            <p>No matching posts found.</p>
+          ) : (
+            searchResults.map((post) => (
+              <div className="post" key={post._id}>
+                <h3>{post.title}</h3>
+                <div>{post.author.username || "Unknown User"}</div>
+                <div>Description: {post.description}</div>
+                <div>Price: {post.price}</div>
+                <div>Location: {post.location}</div>
+              </div>
+            ))
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+export default SearchBar;
